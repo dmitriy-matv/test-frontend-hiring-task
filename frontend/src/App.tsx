01bfb133c3993b4c4352cb349bfbe0ecc5269d3b
Dmitriy Matviichuk <dmitriy.matvv@gmail.com>
2021-08-12 15:23:46 +0300

    [add] navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,8 @@
-import React from 'react';
-
+import React from "react";
+import { Navigation } from "./components";
 
 function App() {
-  return (
-    <></>
-  );
+  return <Navigation />;
 }
 
 export default App;
