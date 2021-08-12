51b49b7eb957240b1777357cf45f33007cd4b6d9
Dmitriy Matviichuk <dmitriy.matvv@gmail.com>
2021-08-12 18:46:32 +0300

    [update] main app component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,46 @@
-import React from "react";
-import { Navigation } from "./components";
+import { Container } from "@material-ui/core";
+import React, { useState, useEffect } from "react";
+import { DeviceScreen, Navigation, UsersScreen } from "./components";
+import { TabPanel } from "./components/Screens/TabPanel/TabPanel";
+import { ApiService } from "./services/apiService";
+import { IDevice } from "./types/devices";
+import { IUser } from "./types/users";
 
 function App() {
-  return <Navigation />;
+  const service = new ApiService();
+  const [tabIndex, setTabIndex] = useState(0);
+
+  const [users, setUsers] = useState<{ data: IUser[] }>();
+  const [devices, setDevices] = useState<{ data: IDevice[] }>();
+  useEffect(() => {
+    fetchUsers();
+    fetchDevices();
+  }, []);
+  const fetchDevices = async () => {
+    const devices = await service.getDevices<{ data: IDevice[] }>();
+    setDevices(devices);
+  };
+  const fetchUsers = async () => {
+    const users = await service.getUsers<{ data: IUser[] }>();
+    setUsers(users);
+  };
+  const handleTabChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+    setTabIndex(newValue);
+  };
+
+  return (
+    <>
+      <Navigation tabNumber={tabIndex} changeHandler={handleTabChange} />
+      <Container maxWidth="sm">
+        <TabPanel value={tabIndex} index={0}>
+          <DeviceScreen devices={devices} />
+        </TabPanel>
+        <TabPanel value={tabIndex} index={1}>
+          <UsersScreen users={users} />
+        </TabPanel>
+      </Container>
+    </>
+  );
 }
 
 export default App;
