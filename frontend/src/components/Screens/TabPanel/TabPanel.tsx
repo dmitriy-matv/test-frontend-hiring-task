0b14ab4cb4e274fa5e7543a9e733057d0219bec5
Dmitriy Matviichuk <dmitriy.matvv@gmail.com>
2021-08-12 18:44:24 +0300

    [add] tab panel component

---
+++ frontend/src/components/Screens/TabPanel/TabPanel.tsx
@@ -0,0 +1,24 @@
+import { Box } from "@material-ui/core";
+
+export interface TabPanelProps {
+  children?: React.ReactNode;
+  index: any;
+  value: any;
+}
+
+export const TabPanel = (props: TabPanelProps) => {
+  const { children, value, index, ...other } = props;
+
+  console.log("props :>> ", props);
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box p={1}>{children}</Box>}
+    </div>
+  );
+};
